f499fef1518ff4768ada578b5fd9765413e29c0b
Montes, Matias Ezequiel <mati.montes@hotmail.com>
2021-11-28 01:30:22 -0300

    Primer versión del anclaje de la lancha a objetos como postes

--- scenes/scripts/boat.gd
+++ scenes/scripts/boat.gd
@@ -6,7 +6,8 @@ export (float) var max_speed = 20
 var speed = 0.0
 var water_speed = 10.0
 var lever_height = Vector2(0,0) # [0] min pos, [1] max movement
-var selected = false
+var power_selected = false
+var anchored = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,11 +16,15 @@ func _ready():
 	var mid_height = get_viewport().size.y / 2.0
 	$Frame.position = Vector2(get_viewport().size.x - 30.0 * ConfigVariables.overlay_size, mid_height)
 	$Lever.position = Vector2(get_viewport().size.x - 30.0 * ConfigVariables.overlay_size, mid_height + 48 * ConfigVariables.overlay_size)
+	$AnchorButton.position = Vector2(50.0 * ConfigVariables.overlay_size, get_viewport().size.y - 50 * ConfigVariables.overlay_size)
 	$Lever/LeverSprite.position = Vector2.ZERO
 	$Lever/LeverCollision.position = Vector2.ZERO
+	$AnchorButton/AnchorSprite.position = Vector2.ZERO
+	$AnchorButton/AnchorCollision.position = Vector2.ZERO
 	lever_height = Vector2($Lever.position.y, 96 * ConfigVariables.overlay_size)
 	$Lever.scale = Vector2(ConfigVariables.overlay_size,ConfigVariables.overlay_size)
 	$Frame.scale = Vector2(ConfigVariables.overlay_size,ConfigVariables.overlay_size)
+	$AnchorButton.scale = Vector2(ConfigVariables.overlay_size * 1.5,ConfigVariables.overlay_size * 1.5)
 	
 	# Initialize water speed
 	water_speed = LevelVariables.water_speed
@@ -27,32 +32,43 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-		power_up(delta)
-	if Input.is_action_pressed("ui_down"):
-		power_down(delta)
-	if Input.is_action_pressed("ui_right") and speed != 0.0:
-		rotate(delta)
-	if Input.is_action_pressed("ui_left") and speed != 0.0:
-		rotate(-delta)
-	if Input.get_accelerometer().x > 3.0:
-		rotate(delta)
-	if Input.get_accelerometer().x < -3.0:
-		rotate(-delta)
-	var movement = Vector2()
-	if speed != 0.0:
-		movement += Vector2(1, 0).rotated($Boat.rotation - PI / 2) * speed
-	movement.y += water_speed
-	$Boat.position += movement * delta * 10
+	if not anchored:
+		# Movement
+		if Input.is_action_pressed("ui_up"):
+			power_up(delta)
+		if Input.is_action_pressed("ui_down"):
+			power_down(delta)
+		if Input.is_action_pressed("ui_right") and speed != 0.0:
+			rotate(delta)
+		if Input.is_action_pressed("ui_left") and speed != 0.0:
+			rotate(-delta)
+		if Input.get_accelerometer().x > 3.0:
+			rotate(delta)
+		if Input.get_accelerometer().x < -3.0:
+			rotate(-delta)
+		var movement = Vector2()
+		if speed != 0.0:
+			movement += Vector2(1, 0).rotated($Boat.rotation - PI / 2) * speed
+		movement.y += water_speed
+		$Boat.move_and_collide(movement * delta * 10)
 
 
 func _on_Lever_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("ui_click"):
-		selected = true
+		power_selected = true
+
+
+func _on_AnchorButton_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("ui_click"):
+		if anchored:
+			anchored = false
+		else:
+			if not $Boat/InteractionArea.get_overlapping_bodies().empty():
+				anchored = true
 
 
 func _physics_process(delta):
-	if selected:
+	if power_selected:
 		if get_global_mouse_position().y < $Lever.position.y:
 			power_up(delta)
 		else:
@@ -62,7 +78,7 @@ func _physics_process(delta):
 func _input(event):
 	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
-			selected = false
+			power_selected = false
 
 
 func power_up(delta):
@@ -85,3 +101,4 @@ func update_lever_y():
 
 func update_water_speed():
 	water_speed = LevelVariables.water_speed
+
